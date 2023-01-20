f99844eb6991ba093efdb1273bf5248aa2b9bffc
Jbsmall17 <alaomiyaki17@gmail.com>
2023-01-20 14:23:53 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Tabs component
+    </div>
+  )
+}
+
+export default App

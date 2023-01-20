d12455015e4235bcf5a011ded5d7d8dd397496d5
Jbsmall17 <alaomiyaki17@gmail.com>
2023-01-20 14:38:28 -0800

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React,{ useState } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+import "./index.css"
+
+const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading,setLoading] = useState(false)
 
+  if(loading){
+    return <h1 className='loading'>Loading...</h1>
+  }
   return (
-    <div className="App">
-      Tabs component
-    </div>
+    <section className="section">
+      <div className="title">
+        <h1>Experience</h1>
+        <div className="underline"></div>
+      </div>
+    </section>
   )
 }
 
